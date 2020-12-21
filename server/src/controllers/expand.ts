385efc6272ee2b10ae4d6347dca5545d8a580a80
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-12-21 21:12:56 +0000

    Implement redirect;

--- server/src/controllers/expand.ts
+++ server/src/controllers/expand.ts
@@ -1,7 +1,30 @@
 import { RequestHandler, Request, Response, NextFunction } from 'express';
 
-//import { decode, fetch } from './../models/shortUrl';
+import { fetch } from './../models/shortUrl';
 
-export const expandURL: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
-    res.sendStatus(501);
+export const expandURL: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const short = req.params.short;
+    if(!short) {
+        console.warn('Missing short id :O');
+        res.sendStatus(400);
+        return;
+    }
+
+    const origin = await fetch(short).catch((err) => {
+        if (err) {
+            console.warn('Failed to fetch original url from redis');
+            res.sendStatus(500);
+            next(err);
+            return;
+        }
+    });
+
+    if(!origin) {
+        res.sendStatus(404);
+        next();
+        return;
+    }
+
+    res.redirect(origin as string);
+    return;
 }
